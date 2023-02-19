6c4ff49878dbd1523a190e5f397e4e228eb3798d
Sara Iza <sasita.iza@gmail.com>
2023-02-18 23:46:28 -0500

    Added Rating domain data class

---
+++ app/src/main/java/com/example/theyelpapp/api/domain/Rating.kt
@@ -0,0 +1,27 @@
+package com.example.theyelpapp.api.domain
+
+import com.example.theyelpapp.api.model.ratings.Review
+
+/**
+ * Data class that defines a rating
+ */
+data class Rating(
+    val id: String = "",
+    val rating: Int = 0,
+    val text: String = "",
+    val timeCreated: String = "",
+    val userImg: String?,
+    val userName: String = ""
+)
+
+fun List<Review>?.mapToRating(): List<Rating> =
+    this?.map {
+        Rating(
+            it.id ?: "",
+            it.rating ?: 0,
+            it.text ?: "",
+            it.timeCreated ?: "",
+            it.user?.imageUrl,
+            it.user?.name ?: ""
+        )
+    }?: emptyList()
